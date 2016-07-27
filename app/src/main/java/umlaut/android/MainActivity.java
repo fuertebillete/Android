f5fc9e8813f090c642c14498ab4cd29dafebb39f
ShiroAmakusa <gabriel92.rojas@gmail.com>
2016-07-27 19:28:29 -0300

    Version estable 1.0
    Fixed backStack:
    -PerfilFragment (linea agregada en la funcion goToLogin())
    -MainActivity (funcionalidad agregada al boton cerrar sesion)

--- app/src/main/java/umlaut/android/MainActivity.java
+++ app/src/main/java/umlaut/android/MainActivity.java
@@ -1,13 +1,186 @@
 package umlaut.android;
 
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true); // Activar/Desactivar menu
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            setupNavigationDrawerContent(navigationView);
+        }
+
+        setupNavigationDrawerContent(navigationView);
+
+        //First start (Login Fragment)
+
+        setFragment(0);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_navigation_drawer_inicio:
+                                menuItem.setChecked(true);
+                                setFragment(1);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_perfil:
+                                menuItem.setChecked(true);
+                                setFragment(2);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_calendario:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_consulta:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_aviso:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_archivo:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_ajustes:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                                startActivity(intent);
+                                return true;
+                            case R.id.item_navigation_drawer_info:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                String link = "http://www.umlaut.com.ar";
+                                Intent pag = null;
+                                pag = new Intent(pag.ACTION_VIEW, Uri.parse(link));
+                                startActivity(pag);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_sesion:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Cerrando Sesión", Toast.LENGTH_SHORT).show();
+                                logout();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    public void setFragment(int position) {
+
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+
+            case 0:
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                LoginFragment loginFragment = new LoginFragment();
+                fragmentTransaction.replace(R.id.fragment_frame, loginFragment,"login");
+
+                fragmentTransaction.commit();
+
+                break;
+
+            case 1:
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                InicioFragment inicioFragment = new InicioFragment();
+                fragmentTransaction.replace(R.id.fragment_frame, inicioFragment,"inicio");
+                fragmentTransaction.addToBackStack("login");
+                fragmentTransaction.commit();
+
+                break;
+
+            case 2:
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                PerfilFragment perfilFragment = new PerfilFragment();
+                fragmentTransaction.replace(R.id.fragment_frame, perfilFragment,"perfil");
+                fragmentTransaction.addToBackStack("inicio");
+                fragmentTransaction.commit();
+
+                break;
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0 ){
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void logout(){
+
+        Intent i = new Intent(MainActivity.this, LoginFragment.class);
+        // set the new task and clear flags
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        setFragment(0);
+
+    }
+
 }
